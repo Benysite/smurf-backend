0a3361d53ba8068fd83ad638ab790c1768e1ecb0
Beny <benjamin2406.vtlbm@gmail.com>
2025-12-06 14:04:46 +0100

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const PlayerSchema = new mongoose.Schema({
-    puuid: { type: String, unique: true, required: true },
+const playerSchema = new mongoose.Schema({
+    puuid: { type: String, unique: true },
     gameName: String,
     tagLine: String,
     shard: String,
     lastSeen: Date
 });
 
-module.exports = mongoose.model("Player", PlayerSchema);
+module.exports = mongoose.model("Player", playerSchema);
