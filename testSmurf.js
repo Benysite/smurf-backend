bacfa67ff7682216937356ce76b5b7b52fc8aa70
Beny <benjamin2406.vtlbm@gmail.com>
2025-12-06 13:22:06 +0100

    Update testSmurf.js

--- testSmurf.js
+++ testSmurf.js
@@ -1,1 +1,15 @@
+const { calculateSmurfScore } = require("./utils/smurfScore");
+
+console.log(
+    calculateSmurfScore({
+        level: 15,
+        accountAgeDays: 5,
+        kd: 1.8,
+        winrate: 70,
+        acs: 260,
+        matchesPlayed: 20,
+        hsRate: 22,
+        rank: null
+    })
+);
 
